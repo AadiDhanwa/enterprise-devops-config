80110f4a47ffcb9492b1bbc5dacd03c99b3aeb76
Mohd Aadil <105550399+AadiDhanwa@users.noreply.github.com>
2026-02-25 06:20:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bank Enterprise App Running"
+    return "return "CI/CD FULLY WORKING — AADIL DEVOPS"
 
 app.run(host="0.0.0.0", port=5000)
